32776a1db78622e5328ff5b0476492b15e14c965
ghkdwlgns612 <ghkdwlgns612@naver.com>
2024-03-02 11:55:35 +0900

    create compensation request

---
+++ outbox-pattern/kafka-object/src/main/java/com/example/ordertopayment/PaymentCompensationRequest.java
@@ -0,0 +1,15 @@
+package com.example.ordertopayment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentCompensationRequest {
+    private String username;
+    private long price;
+}
